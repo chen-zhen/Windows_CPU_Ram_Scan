65acd3bb036a5e64f904804ad535a46243c02ac4
bz <bbo78952123456@gmail.com>
2019-08-12 01:32:43 +0800

    cpu ram program

---
+++ CPURamScan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CPURamScan
+{
+	class Program
+	{
+		static PerformanceCounter cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+		static PerformanceCounter memory = new PerformanceCounter("Memory", "% Committed Bytes in Use");
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("CPU: {0:n1}%", cpu.NextValue());
+				Console.WriteLine("Memory: {0:n0}%", memory.NextValue());
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
